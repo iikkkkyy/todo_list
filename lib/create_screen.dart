a19f417a21425749151d065410d110fe3ff49990
kimkangtae <tt997768@gmail.com>
2023-12-26 17:41:19 +0900

    DB에 TextField에서 입력받은 Todo값 저장

--- lib/create_screen.dart
+++ lib/create_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/main.dart';
+import 'package:todolist/todo.dart';
 
-class CreateScreen extends StatelessWidget {
+class CreateScreen extends StatefulWidget {
   const CreateScreen({super.key});
+
+  @override
+  State<CreateScreen> createState() => _CreateScreenState();
+}
+
+class _CreateScreenState extends State<CreateScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,21 +24,33 @@ class CreateScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Todo 작성'),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.done),)
+          IconButton(
+            onPressed: () async {
+              await todos.add(Todo(
+                title: _textController.text,
+                dateTime: DateTime.now().millisecondsSinceEpoch,
+              ));
+
+              if (mounted) {
+                Navigator.pop(context);
+              }
+            },
+            icon: const Icon(Icons.done),
+          )
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: TextField(
+          controller: _textController,
           decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            hintStyle: TextStyle(color: Colors.grey[800]),
-            hintText: '할일을 입력하세요',
-            filled: true,
-            fillColor: Colors.white70
-          ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              hintStyle: TextStyle(color: Colors.grey[800]),
+              hintText: '할일을 입력하세요',
+              filled: true,
+              fillColor: Colors.white70),
         ),
       ),
     );
