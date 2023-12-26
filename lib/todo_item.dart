6858981be1a7df610772eb465d4261cd295303fb
kimkangtae <tt997768@gmail.com>
2023-12-26 18:31:12 +0900

    todolist style 변경 및 callback 구현

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:todolist/todo.dart';
 
 class TodoItem extends StatelessWidget {
   final Todo todo;
+  final Function(Todo) onTap;
 
   const TodoItem({
     super.key,
     required this.todo,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
+    print(todo.dateTime);
     return ListTile(
-      title: Text(todo.title),
-      subtitle: Text('${todo.dateTime}'),
+      onTap: () {
+        onTap(todo);
+      },
+      leading: todo.isDone
+          ? const Icon(Icons.check_circle, color: Colors.green)
+          : const Icon(Icons.check_circle_outline),
+      title: Text(
+        todo.title,
+        style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),
+      ),
+      subtitle: Text(
+          DateFormat.yMMMMd().format(DateTime.fromMillisecondsSinceEpoch(todo.dateTime)),
+          style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black)),
     );
   }
 }
