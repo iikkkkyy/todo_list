90500c29a60828c52a6c0fb04331b3b705f06830
kimkangtae <tt997768@gmail.com>
2023-12-26 17:30:07 +0900

    DB에 있는 정보 불러오기

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:todolist/create_screen.dart';
 import 'package:todolist/todo.dart';
 
+import 'main.dart';
+
 class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
 
@@ -11,11 +13,7 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  final todos = [
-    Todo(title: 'title 1', dateTime: 12321),
-    Todo(title: 'title 2', dateTime: 113123),
-    Todo(title: 'title 3', dateTime: 1112312133123),
-  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +22,7 @@ class _ListScreenState extends State<ListScreen> {
         title: const Text('Todo 리스트'),
       ),
       body: ListView(
-        children: todos
+        children: todos.values
         .map((todo) => ListTile(
           title: Text(todo.title),
           subtitle: Text('${todo.dateTime}'),
